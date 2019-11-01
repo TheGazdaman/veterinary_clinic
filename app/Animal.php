40dd44dfa35bdfe919921c4b60b6c5f9cadc5c53
Paul Coltofeanu <paulcolto@gmail.com>
2019-11-01 10:42:05 +0100

    seeder

--- app/Animal.php
+++ app/Animal.php
@@ -2,9 +2,17 @@
 
 namespace App;
 
+use App\Doctor;
+use App\Owner;
 use Illuminate\Database\Eloquent\Model;
 
 class Animal extends Model
 {
-    //
+    public function doctor() {
+        return $this->belongsTo('App\Doctor');
+    }
+
+    public function owner() {
+        return $this->belongsTo('App\Owner');
+    }
 }
