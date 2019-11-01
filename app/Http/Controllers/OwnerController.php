1cd1dcd41b3be878c79fe010185bfda69357e1bf
Michal Gazdik <gazdikm@seznam.cz>
2019-11-01 11:48:45 +0100

    API

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Owner;
 
 use Illuminate\Http\Request;
 
 class OwnerController extends Controller
 {
-    //
+    public function index() {
+        $owners = Owner::orderBy('surname', 'ASC')->limit(20)->with('animals')->get();
+        return $owners;
+    }
 }
--- app/Owner.php
+++ app/Owner.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App;
+use App\Animal;
+
 
 use Illuminate\Database\Eloquent\Model;
 
 class Owner extends Model
 {
-    //
+    public function animals() 
+    {
+        return $this->hasMany('App\Animal');
+    }
 }
